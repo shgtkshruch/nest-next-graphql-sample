5b4e51541b05187acf9e025ed95fa7f595272685
shgtkshruch <shgtk.shruch@gmail.com>
2022-07-29 17:39:33 +0900

    Add graphql endpoint
    https://docs.nestjs.com/graphql/quick-start#schema-first

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CatsController } from './cats.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
