08963278781b98749cf7bb12e2e36a56c5a3a976
shgtkshruch <shgtk.shruch@gmail.com>
2022-07-30 20:30:03 +0900

    Add todo by react form with GraphQL mutation

--- next/pages/todos/index.tsx
+++ next/pages/todos/index.tsx
@@ -1,21 +1,31 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import React from 'react'
+import React, { FormEvent } from 'react'
 import { useGetTodosQuery, useCreateTodoMutation } from '../../src/generated/graphql';
 
 export const Todos: NextPage = () => {
-  const { loading, error, data } = useGetTodosQuery();
+  const { loading, error, data, refetch } = useGetTodosQuery();
   const [createTodoMutation] = useCreateTodoMutation();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  function createTodo() {
-    createTodoMutation({
+  async function createTodo(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/
+    const target = e.target as typeof e.target & {
+      title: { value: string };
+    };
+
+    await createTodoMutation({
       variables: {
-        title: 'test1'
+        title: target.title.value
       }
     });
+
+    target.title.value = '';
+    refetch()
   }
 
   return (
@@ -23,7 +33,15 @@ export const Todos: NextPage = () => {
       <Head>
         <title>Todo | Create Next App</title>
       </Head>
-      <button onClick={createTodo}>Add Todo</button>
+
+      <form onSubmit={createTodo}>
+        <label>
+          Title:
+          <input type="text" name="title" />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+
       <ul>
         {data!.todos.map(todo=> (
           <li key={todo.id}>
